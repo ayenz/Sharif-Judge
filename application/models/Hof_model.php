2f561ea267597ed6142835478bd6592f22c0fe58
Stillmen Vallian <stillmen.v@gmail.com>
2018-03-24 22:37:50 +0700

    Inital back-end for Hall of Fame page

--- application/models/Hof_model.php
+++ application/models/Hof_model.php
@@ -1,42 +1,34 @@
 <?php
 /**
  * SharIF Judge online judge
- * @file Hof.php
+ * @file Hof_model.php
  * @author Stillmen Vallian <stillmen.v@gmail.com>
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Hof extends CI_Controller
+class Hof_model extends CI_Model
 {
-
 
 	public function __construct()
 	{
 		parent::__construct();
-		if ( ! $this->session->userdata('logged_in')) // if not logged in
-			redirect('login');
-		$this->load->model('hof_model');
 	}
-
-
 
 
 	// ------------------------------------------------------------------------
 
 
-
-
-	public function index()
+	/**
+	 * Get data for Hall of Fame
+	 *
+	 *
+	 * @return mixed
+	 */
+	public function get_all_final_submission()
 	{
-		$data = array(
-			'hofs' => $this->hof_model->get_all_final_submission()
-		);
-		$this->twig->display('pages/hof.twig', $data);
+    return $this->db->query("SELECT username, SUM(pre_score * coefficient / 100) AS totalscore FROM shj_submissions WHERE is_final=1 GROUP BY username")->result_array();
 	}
 
 
-
-
 	// ------------------------------------------------------------------------
-
 }
